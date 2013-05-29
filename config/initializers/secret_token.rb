4c33139c7788c98bc3a5204bdb5e80056d9aed3a
unknown <shridhar.tikandar@B4L-F24HVS1.gdnindia.com>
2013-05-29 15:00:48 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omsai::Application.config.secret_token = '3be00092e56f1a63e7b1d407485ec43eabd9d653431a44eb2095916665fc4f8f54471293dd7d182fcca6ed77eafd503c7c6b95bea58f27e6b63bf1db628f1583'
